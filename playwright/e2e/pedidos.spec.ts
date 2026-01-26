0453ecc2a0df83c252e903b41334253acbff9a1a
igormullern <igor.natal@hotmail.com>
2026-01-26 20:13:03 -0300

    desafio: sobreviva sem data test ids

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -3,6 +3,8 @@ import { test, expect } from '@playwright/test'
 
 
 test('deve consultar um pedido aprovado', async ({ page }) => {
+  const numeroPedido = 'VLO-MIT3P9'
+
   //Arrange
   await page.goto('http://localhost:5173/')
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
@@ -10,13 +12,12 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
   //Act
-  await page.getByTestId('search-order-id').fill('VLO-MIT3P9')
-  await page.getByTestId('search-order-button').click()
+  await page.getByLabel('Número do Pedido').fill(numeroPedido)
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click()
   
   //Assert
-  await expect(page.getByTestId('order-result-id')).toBeVisible()  
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-MIT3P9')
+  await expect(page.getByText(numeroPedido)).toBeVisible({timeout: 10_000})
+  await expect(page.getByTestId('order-result-'+ numeroPedido)).toContainText(numeroPedido);
   
-  await expect(page.getByTestId('order-result-status')).toBeVisible()
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')  
-})
+  await expect(page.getByText('APROVADO')).toBeVisible()
+})  
